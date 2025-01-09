3555a97ee66adcbfc8d498f5763d6e44e1780d23
Luis Soto Medina <luissoto.personal@gmail.com>
2025-01-09 10:08:59 +0100

    Midi preprocessing Part I
    Incorporation of part of the midi file processing and creation of run_preprocessing.py

---
+++ src/preprocessing/midi_preprocessing.py
@@ -0,0 +1,91 @@
+import mido
+
+def load_midi(midi_path):
+    """
+    Load a midi file
+    
+    Input:
+        midi_path: str, path to the midi file
+    
+    Output:
+        midi_data: mido.MidiFile, midi data
+    """
+    try:
+        midi_data = mido.MidiFile(midi_path)    
+        return midi_data
+    
+    except Exception as e:
+        print(f"Error loading {midi_path}: {e}")
+        return None
+    
+def get_note_density(mid):
+    """
+    Get note density of a midi file
+    
+    Input:
+        mid: mido.MidiFile, midi data
+
+    Output:
+        density: float, note density
+    """
+    duration = mid.length
+    n_notes = sum([1 for track in mid.tracks for msg in track if msg.type == 'note_on'])
+    density = n_notes / duration
+    return density
+
+def get_tempo(mid):
+    """
+    Get tempo of a midi file
+
+    Input:
+        mid: mido.MidiFile, midi data
+    
+    Output:
+        tempo: float, tempo
+    """
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == 'set_tempo':
+                tempo_bpm = mido.tempo2bpm(msg.tempo)
+                return tempo_bpm
+    return None
+
+def get_n_instruments(mid):
+    """
+    Get number of instruments of a midi file
+
+    Input:
+        mid: mido.MidiFile, midi data
+
+    Output:
+        n_instruments: int, number of instruments
+    """
+    instrument_channels = set()
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == 'note_on' and msg.velocity > 0:
+                instrument_channels.add(msg.channel)
+    n_instruments = len(instrument_channels)
+    return n_instruments
+
+def get_midi_features(mid):
+    """
+    Get features of a midi file
+
+    Input:
+        mid: mido.MidiFile, midi data
+    
+    Output:
+        midi_features: dict, midi features
+    """
+    note_density = get_note_density(mid)
+    tempo = get_tempo(mid)
+    n_instruments = get_n_instruments(mid)
+    duration = mid.length
+    midi_features = {
+        "note_density": note_density,
+        "tempo": tempo,
+        "n_instruments": n_instruments,
+        "duration": duration,
+    }
+    return midi_features
---
+++ src/preprocessing/run_preprocessing.py
@@ -0,0 +1,53 @@
+import os
+import json
+import pandas as pd
+from midi_preprocessing import *
+from metadata_preprocessing import *
+
+def save_to_json(metadata_list, output_path, is_midi=True):
+    """
+    Save combined metadata to a JSON file
+
+    Input:
+        metadata_list: list of pd.DataFrame, list of metadata
+        output_path: str, path to save the metadata
+    """
+    if is_midi:
+        combined_metadata = pd.DataFrame(metadata_list)
+    else:
+        combined_metadata = pd.concat(metadata_list, ignore_index=True)
+    metadata_dict = combined_metadata.to_dict(orient='records')
+    with open(output_path, 'w') as f:
+        json.dump(metadata_dict, f, indent=4)
+        print(f"Metadata saved to {output_path}")
+
+
+if __name__=='__main__':
+    raw_data_path = './data/raw'
+    processed_data_path = './data/processed'
+    csv_metadata_list = []
+    midi_metadata_list = []
+
+    files = os.listdir(raw_data_path)
+    for filename in files:
+        file_path = os.path.join(raw_data_path, filename)
+        print(f"Processing {filename}")
+        if filename.endswith('.mid'):
+            midi_file = load_midi(file_path)
+            if midi_file is not None:
+                midi_features = get_midi_features(midi_file)
+                metadata = {
+                    "name": filename,
+                    **midi_features
+                }
+                midi_metadata_list.append(metadata)
+        
+        if filename.endswith('.csv'):
+            csv_metadata = load_metadata(file_path)
+            if csv_metadata is not None:
+                processed_metadata = process_metadata(csv_metadata)
+                csv_metadata_list.append(processed_metadata)
+
+    save_to_json(midi_metadata_list, os.path.join(processed_data_path, 'metadata_midi.json'))
+    save_to_json(csv_metadata_list, os.path.join(processed_data_path, 'metadata_csv.json'), is_midi=False)
+
